880a719c2b7ea1bbceb80e5c5d6c3442a1b72f88
slavvvvva <slavvvvva@yandex.ua>
2020-10-20 21:44:28 +0200

    i am stupid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import s from './App.module.scss';
+import { Container, Row, Col } from 'reactstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className ={s.body}  fluid={true}>
+    <Row>
+      <Col className ={s.head}>Щапка</Col>
+    </Row>
+    <Row>
+      <Col className ={s.navig} xs="2">меню</Col>
+      <Col xs="auto">место пользователя</Col>
+    </Row>
+    <Row>
+      <Col className ={s.foot}>подвал</Col>
+    </Row>
+  </Container>
   );
 }
 
