d17b20f96657d0089f28edf374d529473386773c
slavvvvva <slavvvvva@yandex.ua>
2020-10-25 02:57:21 +0100

    hz cho ya tam dobavil

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './App.module.scss';
 import { Container, Row, Col } from 'reactstrap';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header/header';
 import Nawigation from './components/Navigation/nawigation';
 import User from './components/User/user';
@@ -10,24 +10,24 @@ import Chat from './components/Massedges/masseges';
 
 
 function App() {
-  return (
-     <BrowserRouter>
-         <Container  className ={s.body}>
-             <Row>
-               <Col className ={s.head}><Header/></Col>
+   return (
+      <BrowserRouter>
+         <Container className={s.body}>
+            <Row>
+               <Col className={s.head}><Header /></Col>
             </Row>
-            <Row className = {s.content}>
-               <Nawigation/>
-               <Route path = '/meseges' component = {Chat} />
-               <Route path = '/myprofile' component = {User} />
+            <Row className={s.content}>
+               <Nawigation />
+               <Route path='/meseges' component={Chat} />
+               <Route path='/myprofile' component={User} />
             </Row>
             <Row>
-               <Col className ={s.foot}>подвал</Col>
+               <Col className={s.foot}>подвал</Col>
             </Row>
          </Container>
-     </BrowserRouter>
-      
-  );
+      </BrowserRouter>
+
+   );
 }
 
 export default App;
--- src/components/Navigation/nawigation.js
+++ src/components/Navigation/nawigation.js
@@ -2,22 +2,23 @@ import React, { useState } from 'react';
 import n from './n.module.scss';
 import { Col } from 'reactstrap';
 import { ListGroup, ListGroupItem, Badge } from 'reactstrap';
+import { NavLink } from 'react-router-dom'
 
 const Nawigation = () => {
 
-    let newClass = `justify-content-between ${n.list} `
-    let last = `justify-content-between ${n.list} ${n.last}`
+  let newClass = `justify-content-between ${n.list} `
+  let last = `justify-content-between ${n.list} ${n.last}`
 
   return (
-    <Col className ={n.navig} xs="2">
-      <ListGroup className ={n.list_groop} >
-        <ListGroupItem className={newClass} > <a href ="/myprofile" >Моя страница <Badge pill>1</Badge></a> </ListGroupItem>
-        <ListGroupItem className={newClass}> <a href ="/frends" >Друзья <Badge pill>2</Badge></a></ListGroupItem>
-        <ListGroupItem className={newClass}> <a href ="/meseges" >Сообщения <Badge pill>11</Badge></a> </ListGroupItem>
-        <ListGroupItem className={newClass}> <a href ="/news" >Новости <Badge pill>22</Badge></a></ListGroupItem>
+    <Col className={n.navig} xs="2">
+      <ListGroup className={n.list_groop} >
+        <ListGroupItem className={newClass} > <NavLink to="/myprofile" activeClassName={n.activ_link} >Моя страница <Badge pill>1</Badge></NavLink> </ListGroupItem>
+        <ListGroupItem className={newClass}> <NavLink to="/frends" activeClassName={n.activ_link} >Друзья <Badge pill>2</Badge></NavLink></ListGroupItem>
+        <ListGroupItem className={newClass}> <NavLink to="/meseges" activeClassName={n.activ_link} >Сообщения <Badge pill>11</Badge></NavLink> </ListGroupItem>
+        <ListGroupItem className={newClass}> <NavLink to="/news" activeClassName={n.activ_link} >Новости <Badge pill>22</Badge></NavLink></ListGroupItem>
         <ListGroupItem className={last}></ListGroupItem>
-        <ListGroupItem className={newClass}> <a href ="/settings" >Настройки <Badge pill>1</Badge></a></ListGroupItem>
-    </ListGroup>
+        <ListGroupItem className={newClass}> <NavLink to="/settings" activeClassName={n.activ_link} >Настройки <Badge pill>1</Badge></NavLink></ListGroupItem>
+      </ListGroup>
     </Col>
   )
 }
