6132013fd8da8d5f16d471ccbdf9dd454975d705
Вячеслав Журавель <slavvvvva@yandex.ua>
2020-12-15 20:15:18 +0100

    add lazy load for frands page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { Row, Col } from 'reactstrap';
 import { BrowserRouter, Route, withRouter } from 'react-router-dom';
 import s from './App.module.scss';
@@ -6,12 +6,13 @@ import HeaderContainer from './components/Header/headerContainer';
 import Nawigation from './components/Navigation/nawigation';
 import UserContainer from './components/User/user-container';
 import ChatContainer from './components/Massedges/messeges.container'
-import FrendsContainer from './components/Frends/frendes-container'
+//import FrendsContainer from './components/Frends/frendes-container'
 import Login from './components/Login/login'
 import {initializedTC} from './Redax/app-reduser'
 import { connect } from 'react-redux'
 import { compose } from 'redux';
 import Loader from './components/Loader/loader';
+const  FrendsContainer  = lazy( () => import('./components/Frends/frendes-container'))
 
 class App extends React.Component {
    componentDidMount() {
@@ -29,7 +30,7 @@ class App extends React.Component {
                   <Nawigation />
                   <Route path='/meseges' render={() => <ChatContainer />} />
                   <Route path='/myprofile/:userId?' render={() => <UserContainer />} />
-                  <Route path='/frends' render={() => <FrendsContainer />} />
+                  <Route path='/frends' render={() => <React.Suspense fallback ={<Loader/>}>  <FrendsContainer /> </React.Suspense> } />
                   <Route path='/login' render={() => <Login />} />
                </Row>
                <Row>
